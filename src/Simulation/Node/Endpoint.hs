df5ae9d86a997b94d1366efdfaeda349bd9106e6
Patrik Sandahl <kosmoskatten.software@gmail.com>
2014-07-19 21:02:57 +0200

    Simplified the endpoint.

--- src/Simulation/Node/Endpoint.hs
+++ src/Simulation/Node/Endpoint.hs
@@ -5,77 +5,20 @@ module Simulation.Node.Endpoint
        , destroy
        ) where
 
-import Control.Applicative ((<$>), (<*>))
-import Control.Concurrent.Async (Async, async, waitCatch)
-import Control.Concurrent.STM
-  ( TChan
-  , atomically
-  , newTChanIO
-  , readTChan
-  , writeTChan
-  )
-
 type IpAddress = String
 
 -- | An endpoint instance descriptor.
 data Endpoint =
-  Endpoint { ipAddress   :: !IpAddress
-           , commandChan :: TChan Command
-           , replyChan   :: TChan Reply
-           , task        :: Async () }
+  Endpoint { ipAddress :: !IpAddress }
   deriving Eq
-
--- | Commands sent from the API to the endpoint thread.
-data Command =
-    CreateReq !Endpoint
-  | DestroyReq
-
--- | Replies from the endpoint thread back to the API.
-data Reply =
-  CreateConf
 
 -- | Create an endpoint instance.
 create :: IpAddress -> IO Endpoint
-create theIpAddress = do
-  cchan <- newTChanIO
-  ep <- Endpoint theIpAddress cchan <$> newTChanIO <*> async (endpoint cchan)
-  command ep $ CreateReq ep
-  CreateConf <- receive $ replyChan ep
-  return ep
+create theIpAddress = return $ Endpoint theIpAddress
 
 -- | Destroy an endpoint instance.
 destroy :: Endpoint -> IO ()
-destroy ep = do
-  command ep DestroyReq
-  _ <- waitCatch (task ep)
-  return ()
+destroy _ = return ()
 
--- | The entry for the endpoint thread.
-endpoint :: TChan Command -> IO ()
-endpoint cchan = do
-  CreateReq ep <- receive cchan
-  reply ep CreateConf
-  eventLoop ep
-
--- | Event loop for the endpoint thread
-eventLoop :: Endpoint -> IO ()
-eventLoop ep = do
-  request <- receive (commandChan ep)
-  return ()
-
-command :: Endpoint -> Command -> IO ()
-command ep = writeTChanIO (commandChan ep)
-
-reply :: Endpoint -> Reply -> IO ()
-reply ep = writeTChanIO (replyChan ep)
-
-receive :: TChan a -> IO a
-receive = readTChanIO
-
-readTChanIO :: TChan a -> IO a
-readTChanIO chan = atomically $ readTChan chan
-  
-writeTChanIO :: TChan a -> a -> IO ()
-writeTChanIO chan value = atomically $ writeTChan chan value
   
   
