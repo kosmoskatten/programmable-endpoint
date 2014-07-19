8919e71a0a199314448db4a0413003d28c4f6bcb
Patrik Sandahl <kosmoskatten.software@gmail.com>
2014-07-19 22:37:42 +0200

    Failing unit test for adding a behavior.

--- src/Simulation/Node/Endpoint.hs
+++ src/Simulation/Node/Endpoint.hs
@@ -1,24 +1,53 @@
 module Simulation.Node.Endpoint
        ( Endpoint
+       , Receipt
        , IpAddress
        , create
        , destroy
+       , addBehavior
+       , removeBehavior
        ) where
 
+import Control.Applicative ((<$>), (<*>))
+import Control.Concurrent.STM (TVar, newTVarIO)
+import qualified Data.Map.Strict as Map
+import Simulation.Node.Endpoint.Behavior
+  ( Behavior
+  , BehaviorState
+  )
+
 type IpAddress = String
+type BehaviorMap = Map.Map Receipt Int
 
 -- | An endpoint instance descriptor.
 data Endpoint =
-  Endpoint { ipAddress :: !IpAddress }
+  Endpoint { ipAddress      :: !IpAddress
+           , receiptCounter :: TVar Int
+           , behaviorMap    :: TVar BehaviorMap
+           }
+  deriving Eq
+
+-- | A receipt for an added behavior.
+newtype Receipt = Receipt Int
   deriving Eq
 
 -- | Create an endpoint instance.
 create :: IpAddress -> IO Endpoint
-create theIpAddress = return $ Endpoint theIpAddress
+create theIpAddress =
+  Endpoint theIpAddress <$> newTVarIO 1 <*> newTVarIO (Map.empty)
 
 -- | Destroy an endpoint instance.
 destroy :: Endpoint -> IO ()
 destroy _ = return ()
+
+-- | Add a behavior to the endpoint.
+addBehavior :: BehaviorState s => Behavior s a -> Endpoint -> IO Receipt
+addBehavior action endpoint = return $ Receipt 1
+
+-- | Remove a behavior from the endpoint.
+removeBehavior :: Receipt -> Endpoint -> IO (Either String ())
+removeBehavior _ _ = return $ Right ()
+
 
   
   
--- test/EndpointTests.hs
+++ test/EndpointTests.hs
@@ -1,23 +1,55 @@
+{-# LANGUAGE OverloadedStrings #-}
 module EndpointTests
        ( suite
        ) where
 
+import Control.Monad (void)
 import Test.Framework (Test, testGroup)
 import Test.Framework.Providers.HUnit (testCase)
 import Test.HUnit
-import Simulation.Node.Endpoint (create, destroy)
+import Data.Text ()
+import Simulation.Node.Endpoint
+  ( create
+  , destroy
+  , addBehavior
+  , removeBehavior
+  )
+import Simulation.Node.Endpoint.Behavior
+  ( Behavior
+  , BehaviorState (..)
+  )
 
 suite :: Test.Framework.Test
 suite = testGroup "Endpoint tests"
-        [ testCase "Shall be different endpoints for different Ip's"
-                   createWithDifferentIpsShallGiveDifferentEndpoints
+        [ testCase "Shall be different endpoints"
+                   shallBeDifferentEndpoints
+        , testCase "Shall be different receipts"
+                   shallBeDifferentReceipts
         ]
 
-createWithDifferentIpsShallGiveDifferentEndpoints :: Assertion
-createWithDifferentIpsShallGiveDifferentEndpoints = do
+data TestState = TestState
+  deriving (Eq, Show)
+
+instance BehaviorState TestState where
+  fetch = return ("TestSlogan", TestState)
+
+emptyAction :: Behavior TestState ()
+emptyAction = return ()
+
+shallBeDifferentEndpoints :: Assertion
+shallBeDifferentEndpoints = do
   ep1 <- create "127.0.0.1"
   ep2 <- create "127.0.0.2"
   assertBool "Shall be different" $ ep1 /= ep2
   destroy ep1
   destroy ep2
+
+shallBeDifferentReceipts :: Assertion
+shallBeDifferentReceipts = do
+  ep <- create "127.0.0.1"
+  r1 <- addBehavior emptyAction ep
+  r2 <- addBehavior emptyAction ep
+  assertBool "Shall be different" $ r1 /= r2
+  void $ removeBehavior r1 ep
+  void $ removeBehavior r2 ep
 
