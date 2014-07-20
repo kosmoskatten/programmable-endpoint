9b71953c1cc58501bf9e26a95ccb73b771e88351
Patrik Sandahl <kosmoskatten.software@gmail.com>
2014-07-20 20:44:00 +0200

    Failing testcase.
    Checking that an added behavior is running in its own thread.

--- test/EndpointTests.hs
+++ test/EndpointTests.hs
@@ -3,20 +3,29 @@ module EndpointTests
        ( suite
        ) where
 
-import Control.Monad (void)
+import Control.Concurrent (threadDelay)
+import Control.Concurrent.STM
+  ( TVar
+  , atomically
+  , modifyTVar
+  , newTVarIO
+  , readTVarIO
+  )
+import Control.Monad (forever)
 import Test.Framework (Test, testGroup)
 import Test.Framework.Providers.HUnit (testCase)
 import Test.HUnit
 import Data.Text ()
+import System.Timeout (timeout)
 import Simulation.Node.Endpoint
   ( create
-  , destroy
   , addBehavior
   , removeBehavior
   )
 import Simulation.Node.Endpoint.Behavior
   ( Behavior
   , BehaviorState (..)
+  , liftIO
   )
 
 suite :: Test.Framework.Test
@@ -27,6 +36,8 @@ suite = testGroup "Endpoint tests"
                    shallBeDifferentReceipts
         , testCase "Shall remove a behavior once"
                    shallRemoveABehaviorOnce
+        , testCase "Shall run a behavior in its own thread"
+                   shallRunInItsOwnThread
         ]
 
 data TestState = TestState
@@ -38,6 +49,12 @@ instance BehaviorState TestState where
 emptyAction :: Behavior TestState ()
 emptyAction = return ()
 
+countingAction :: TVar Int -> Behavior TestState ()
+countingAction tvar =
+  forever $ do
+    liftIO $ atomically (modifyTVar tvar (+ 1))
+    liftIO $ threadDelay 10000 -- 1/100th
+
 -- | Test that two endpoints, for two different, IP addresses are
 -- unequal.
 shallBeDifferentEndpoints :: Assertion
@@ -45,8 +62,6 @@ shallBeDifferentEndpoints = do
   ep1 <- create "127.0.0.1"
   ep2 <- create "127.0.0.2"
   assertBool "Shall be different" $ ep1 /= ep2
-  destroy ep1
-  destroy ep2
 
 -- | Test that two receipts gotten the same endpoint are unequal.
 shallBeDifferentReceipts :: Assertion
@@ -55,9 +70,6 @@ shallBeDifferentReceipts = do
   r1 <- addBehavior emptyAction ep
   r2 <- addBehavior emptyAction ep
   assertBool "Shall be different" $ r1 /= r2
-  void $ removeBehavior r1 ep
-  void $ removeBehavior r2 ep
-  destroy ep
 
 -- | Test that a receipt only can be removed once. The second time an
 -- error message shall be returned.
@@ -70,6 +82,23 @@ shallRemoveABehaviorOnce = do
   assertEqual "Shall be Right ()"
               (Right ()) resultSuccess
   assertBool "Shall be different" $ resultSuccess /= resultFailure
-  destroy ep
-  
 
+-- | Test that an added behavior run in its own thread.
+shallRunInItsOwnThread :: Assertion
+shallRunInItsOwnThread = do
+  ep <- create "127.0.0.1"
+  tvar <- newTVarIO 0
+  maybeR <- timeout 100000 $ addBehavior (countingAction tvar) ep
+  case maybeR of
+    Just r ->
+      assertBool "Counter shall have progressed" =<< isProgressing tvar      
+    _      -> assertBool "addBehavior is blocking - not threaded?" False
+
+-- | Check if a TVar protected counter is progressing during 1/10th of
+-- a second.
+isProgressing :: TVar Int -> IO Bool
+isProgressing tvar = do
+  sample <- readTVarIO tvar
+  threadDelay 100000
+  sample' <- readTVarIO tvar
+  return $ sample' > sample
