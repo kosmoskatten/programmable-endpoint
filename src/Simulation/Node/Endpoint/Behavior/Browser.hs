021890d151b7539688f83dbc60e19f89bd88ce0c
Patrik Sandahl <kosmoskatten.software@gmail.com>
2014-07-27 22:25:33 +0200

    Added very rudimentary browser behavior.

--- src/Simulation/Node/Endpoint/Behavior/Browser.hs
+++ src/Simulation/Node/Endpoint/Behavior/Browser.hs
@@ -1,8 +1,22 @@
-module Simulation.Node.Endpoint.Behavior.Browser where
+module Simulation.Node.Endpoint.Behavior.Browser
+       ( browsePage
+       ) where
 
 import qualified Data.ByteString as BS
 import Network.Http.Client
 import Simulation.Node.Endpoint.Behavior
 
-browseAt :: BehaviorState s => BS.ByteString -> Behavior s BS.ByteString
-browseAt whereTo = return BS.empty
+browsePage :: BehaviorState s => BS.ByteString -> Behavior s BS.ByteString
+browsePage resource = do
+  gateway <- webGateway
+  port    <- webPort
+  page    <- liftIO $ fetchResource gateway port resource
+  return page
+
+fetchResource :: Hostname -> Port -> BS.ByteString -> IO BS.ByteString
+fetchResource hostname port resource =
+  withConnection (openConnection hostname port) $ \conn -> do
+    request <- buildRequest $
+      http GET resource
+    sendRequest conn request emptyBody
+    receiveResponse conn concatHandler
