084fc2a1cc0559c2c3ea748adcf156932bcfccf9
Patrik Sandahl <kosmoskatten.software@gmail.com>
2014-08-03 14:49:27 +0200

    Changed storage from Set to List.

--- src/Simulation/Node.hs
+++ src/Simulation/Node.hs
@@ -14,7 +14,7 @@ import Control.Concurrent.STM
   , newTVarIO
   , readTVarIO
   )
-import qualified Data.Set as Set
+import Data.List (delete)
 import Simulation.Node.Counter
 import qualified Simulation.Node.Counter as Counter
 import Simulation.Node.Endpoint
@@ -24,36 +24,32 @@ import Simulation.Node.Endpoint.Behavior
   , Port
   )
 
-type EndpointSet c = Set.Set (Endpoint c)
-
 -- | A node instance descriptor.
 data Node c =
   Node { webGateway  :: !Hostname
        , webPort     :: !Port
-       , endpointSet :: TVar (EndpointSet c)
-       , counter     :: TVar c}
+       , endpoints   :: TVar [Endpoint c]
+       , counter     :: TVar c }
   deriving Eq
 
 -- | Create a node instance.
 create :: Counter c => Hostname -> Port -> IO (Node c)
-create gateway port = Node gateway port <$> newTVarIO Set.empty
+create gateway port = Node gateway port <$> newTVarIO []
                                         <*> newTVarIO Counter.empty
 
 -- | Create an endpoint instance.
 createEndpoint :: (Ord c, Counter c) => IpAddress -> Node c -> IO (Endpoint c)
 createEndpoint ip node = do
   endpoint <- Endpoint.create ip (webGateway node) (webPort node)
-  atomically $ modifyTVar' (endpointSet node)
-                           (\s -> Set.insert endpoint s)
+  atomically $ modifyTVar' (endpoints node) (endpoint:)
   return endpoint
 
 -- | Remove an endpoint from the node.
 removeEndpoint :: (Ord c, Counter c) => Endpoint c -> Node c -> IO ()
 removeEndpoint endpoint node = do
   reset endpoint
-  atomically $ modifyTVar' (endpointSet node)
-                           (\s -> Set.delete endpoint s)
+  atomically $ modifyTVar' (endpoints node) (delete endpoint)
 
 -- | List all endpoints.
 listAll :: Node c -> IO [Endpoint c]
-listAll node = Set.toList <$> readTVarIO (endpointSet node)
+listAll node = readTVarIO (endpoints node)
